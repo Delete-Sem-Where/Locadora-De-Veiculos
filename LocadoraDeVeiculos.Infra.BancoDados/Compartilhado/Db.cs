53d0cef663e4bfda624ee5802ec91cc0044d9ac7
slackbot115 <viniccius.carvalho22@gmail.com>
2022-06-22 19:05:15 -0300

    Add project files.

---
+++ LocadoraDeVeiculos.Infra.BancoDados/Compartilhado/Db.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+
+namespace LocadoraDeVeiculos.Infra.BancoDados.Compartilhado
+{
+    public static class Db
+    {
+        private static string enderecoBanco =
+            @"Data Source=(localdb)\MSSQLLocalDB;
+            Initial Catalog=LocadoraDeVeiculosDb;
+            Integrated Security=True;";
+
+        public static void ExecutarSql(string sql)
+        {
+            SqlConnection conexaoComBanco = new SqlConnection(enderecoBanco);
+
+            SqlCommand comando = new SqlCommand(sql, conexaoComBanco);
+
+            conexaoComBanco.Open();
+            comando.ExecuteNonQuery();
+            conexaoComBanco.Close();
+        }
+
+    }
+}
---
+++ LocadoraDeVeiculos.WinApp/TelaPrincipalForm.cs
@@ -0,0 +1,176 @@
+﻿using LocadoraDeVeiculos.WinApp.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace LocadoraDeVeiculos.WinApp
+{
+    public partial class TelaPrincipalForm : Form
+    {
+        private ControladorBase controlador;
+        private Dictionary<string, ControladorBase> controladores;
+
+        public TelaPrincipalForm()
+        {
+            InitializeComponent();
+
+            Instancia = this;
+
+            labelRodape.Text = string.Empty;
+            labelTipoCadastro.Text = string.Empty;
+
+            InicializarControladores();
+        }
+
+        public static TelaPrincipalForm Instancia
+        {
+            get;
+            private set;
+        }
+
+        public void AtualizarRodape(string mensagem)
+        {
+            labelRodape.Text = mensagem;
+        }
+
+        private void tarefasMenuItem_Click(object sender, EventArgs e)
+        {
+            ConfigurarTelaPrincipal((ToolStripMenuItem)sender);
+        }
+
+        private void contatosMenuItem_Click(object sender, EventArgs e)
+        {
+            ConfigurarTelaPrincipal((ToolStripMenuItem)sender);
+        }
+
+        private void compromissosMenuItem_Click(object sender, EventArgs e)
+        {
+            ConfigurarTelaPrincipal((ToolStripMenuItem)sender);
+        }
+
+        private void despesasMenuItem_Click(object sender, EventArgs e)
+        {
+            ConfigurarTelaPrincipal((ToolStripMenuItem)sender);
+        }
+
+        private void categoriasMenuItem_Click(object sender, EventArgs e)
+        {
+            ConfigurarTelaPrincipal((ToolStripMenuItem)sender);
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            controlador.Inserir();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            controlador.Editar();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            controlador.Excluir();
+        }
+
+        //private void btnAdicionarItens_Click(object sender, EventArgs e)
+        //{
+        //    controlador.AdicionarItens();
+        //}
+
+        //private void btnAtualizarItens_Click(object sender, EventArgs e)
+        //{
+        //    controlador.AtualizarItens();
+        //}
+
+        //private void btnFiltrar_Click(object sender, EventArgs e)
+        //{
+        //    controlador.Filtrar();
+        //}
+
+        //private void btnAgrupar_Click(object sender, EventArgs e)
+        //{
+        //    controlador.Agrupar();
+        //}
+
+        //private void btnVisualizar_Click(object sender, EventArgs e)
+        //{
+        //    controlador.Visualizar();
+        //}
+
+        private void ConfigurarBotoes(ConfiguracaoToolboxBase configuracao)
+        {
+            btnInserir.Enabled = configuracao.InserirHabilitado;
+            btnEditar.Enabled = configuracao.EditarHabilitado;
+            btnExcluir.Enabled = configuracao.ExcluirHabilitado;
+            btnAdicionarItens.Enabled = configuracao.AdicionarItensHabilitado;
+            btnAtualizarItens.Enabled = configuracao.AtualizarItensHabilitado;
+            btnFiltrar.Enabled = configuracao.FiltrarHabilitado;
+            btnAgrupar.Enabled = configuracao.AgruparHabilitado;
+            btnVisualizar.Enabled = configuracao.VisualizarHabilitado;
+        }
+
+        private void ConfigurarTooltips(ConfiguracaoToolboxBase configuracao)
+        {
+            btnInserir.ToolTipText = configuracao.TooltipInserir;
+            btnEditar.ToolTipText = configuracao.TooltipEditar;
+            btnExcluir.ToolTipText = configuracao.TooltipExcluir;
+            btnAdicionarItens.ToolTipText = configuracao.TooltipAdicionarItens;
+            btnAtualizarItens.ToolTipText = configuracao.TooltipAtualizarItens;
+            btnFiltrar.ToolTipText = configuracao.TooltipFiltrar;
+            btnAgrupar.ToolTipText = configuracao.TooltipAgrupar;
+            btnVisualizar.ToolTipText = configuracao.TooltipVisualizar;
+        }
+
+        private void ConfigurarTelaPrincipal(ToolStripMenuItem opcaoSelecionada)
+        {
+            var tipo = opcaoSelecionada.Text;
+
+            controlador = controladores[tipo];
+
+            ConfigurarToolbox();
+
+            ConfigurarListagem();
+        }
+
+        private void ConfigurarToolbox()
+        {
+            ConfiguracaoToolboxBase configuracao = controlador.ObtemConfiguracaoToolbox();
+
+            if (configuracao != null)
+            {
+                toolbox.Enabled = true;
+
+                labelTipoCadastro.Text = configuracao.TipoCadastro;
+
+                ConfigurarTooltips(configuracao);
+
+                ConfigurarBotoes(configuracao);
+            }
+        }
+
+        private void ConfigurarListagem()
+        {
+            AtualizarRodape("");
+
+            var listagemControl = controlador.ObtemListagem();
+
+            panelRegistros.Controls.Clear();
+
+            listagemControl.Dock = DockStyle.Fill;
+
+            panelRegistros.Controls.Add(listagemControl);
+        }
+
+        private void InicializarControladores()
+        {
+            //var repositorioTarefa = new RepositorioTarefaEmBancoDados();
+
+            //controladores = new Dictionary<string, ControladorBase>();
+
+            //controladores.Add("Tarefas", new ControladorTarefa(repositorioTarefa));
+        }
+
+
+    }
+}
