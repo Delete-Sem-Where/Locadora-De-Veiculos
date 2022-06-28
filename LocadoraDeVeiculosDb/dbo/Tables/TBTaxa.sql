d7e893afcb8d5ca922bbae16498caebafde26f26
Luciano Dalmollin <xlucianox811@gmail.com>
2022-06-28 00:16:13 -0300

    feat: adição de tabela taxa ao projeto  database.

---
+++ LocadoraDeVeiculosDb/dbo/Tables/TBTaxa.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TBTaxa] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Descricao] VARCHAR (150) NULL,
+    [Valor]     FLOAT (53)    NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
