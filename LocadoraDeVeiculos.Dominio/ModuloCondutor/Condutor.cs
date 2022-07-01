ea9df3951f8955cd7754675889fe24eea747175c
slackbot115 <viniccius.carvalho22@gmail.com>
2022-07-01 16:19:24 -0300

    feat: Dominio condutor

---
+++ LocadoraDeVeiculos.Dominio/ModuloCondutor/Condutor.cs
@@ -0,0 +1,43 @@
+﻿using LocadoraDeVeiculos.Dominio.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloCondutor
+{
+    public class Condutor : EntidadeBase<Condutor>
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+        public string CPF { get; set; }
+        public string CNH { get; set; }
+        public int ValidadeCNH { get; set; }
+
+        public Condutor Clonar()
+        {
+            return MemberwiseClone() as Condutor;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            Condutor condutor = obj as Condutor;
+
+            if (condutor == null)
+                return false;
+
+            return
+                condutor.Id.Equals(Id) &&
+                condutor.Nome.Equals(Nome) &&
+                condutor.Email.Equals(Email) &&
+                condutor.Telefone.Equals(Telefone) &&
+                condutor.Endereco.Equals(Endereco) &&
+                condutor.CPF.Equals(CPF) &&
+                condutor.CNH.Equals(CNH) &&
+                condutor.ValidadeCNH.Equals(ValidadeCNH);
+        }
+    }
+}
---
+++ LocadoraDeVeiculos.Dominio/ModuloCondutor/ValidadorCondutor.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using LocadoraDeVeiculos.Dominio.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloCondutor
+{
+    public class ValidadorCondutor : AbstractValidator<Condutor>
+    {
+        public ValidadorCondutor()
+        {
+            RuleFor(x => x.Nome)
+                .NotNull().NotEmpty()
+                .Nomes();
+
+            RuleFor(x => x.CPF)
+                .NotNull().NotEmpty()
+                .CPF();
+
+            RuleFor(x => x.CNH)
+                .NotNull().NotEmpty()
+                .MinimumLength(11);
+
+            RuleFor(x => x.ValidadeCNH)
+                .NotNull().NotEmpty();
+
+            RuleFor(x => x.Email)
+                .NotNull().NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Telefone)
+                .NotNull().NotEmpty()
+                .Telefone();
+
+            RuleFor(x => x.Endereco)
+                .NotNull().NotEmpty()
+                .Endereco();            
+        }
+    }
+}
