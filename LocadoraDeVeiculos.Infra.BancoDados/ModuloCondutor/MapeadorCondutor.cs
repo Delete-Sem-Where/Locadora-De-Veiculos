c097330b29fba4397075c2cbd2f48372d42eb816
slackbot115 <viniccius.carvalho22@gmail.com>
2022-07-01 16:23:28 -0300

    feat: Repositório condutor

---
+++ LocadoraDeVeiculos.Infra.BancoDados/ModuloCondutor/MapeadorCondutor.cs
@@ -0,0 +1,50 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloCondutor;
+using LocadoraDeVeiculos.Infra.BancoDados.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.BancoDados.ModuloCondutor
+{
+    public class MapeadorCondutor : MapeadorBase<Condutor>
+    {
+        public override void ConfigurarParametros(Condutor registro, SqlCommand comando)
+        {
+            comando.Parameters.AddWithValue("ID", registro.Id);
+            comando.Parameters.AddWithValue("NOME", registro.Nome);
+            comando.Parameters.AddWithValue("EMAIL", registro.Email);
+            comando.Parameters.AddWithValue("TELEFONE", registro.Telefone);
+            comando.Parameters.AddWithValue("ENDERECO", registro.Endereco);
+            comando.Parameters.AddWithValue("CPF", registro.CPF);
+            comando.Parameters.AddWithValue("CNH", registro.CNH);
+            comando.Parameters.AddWithValue("VALIDADECNH", registro.ValidadeCNH);
+        }
+
+        public override Condutor ConverterRegistro(SqlDataReader leitorRegistro)
+        {
+            int id = Convert.ToInt32(leitorRegistro["CONDUTOR_ID"]);
+            string nome = Convert.ToString(leitorRegistro["CONDUTOR_NOME"]);
+            string email = Convert.ToString(leitorRegistro["CONDUTOR_EMAIL"]);
+            string telefone = Convert.ToString(leitorRegistro["CONDUTOR_TELEFONE"]);
+            string endereco = Convert.ToString(leitorRegistro["CONDUTOR_ENDERECO"]);
+            string cpf = Convert.ToString(leitorRegistro["CONDUTOR_CPF"]);
+            string cnh = Convert.ToString(leitorRegistro["CONDUTOR_CNH"]);
+            int validade_cnh = Convert.ToInt32(leitorRegistro["CONDUTOR_VALIDADECNH"]);
+
+            return new Condutor()
+            {
+                Id = id,
+                Nome = nome,
+                Email = email,
+                Telefone = telefone,
+                Endereco = endereco,
+                CPF = cpf,
+                CNH = cnh,
+                ValidadeCNH = validade_cnh
+            };
+        }
+    }
+}
---
+++ LocadoraDeVeiculos.Infra.BancoDados/ModuloCondutor/RepositorioCondutorEmBancoDados.cs
@@ -0,0 +1,124 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloCondutor;
+using LocadoraDeVeiculos.Infra.BancoDados.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.BancoDados.ModuloCondutor
+{
+    public class RepositorioCondutorEmBancoDados :
+        RepositorioBase<Condutor, MapeadorCondutor>,
+        IRepositorioCondutor
+    {
+        protected override string sqlInserir =>
+            @"INSERT INTO [TBCONDUTOR] 
+            (
+                [NOME],
+                [EMAIL],
+                [TELEFONE],
+                [ENDERECO],
+                [CPF],
+                [CNH],
+                [VALIDADECNH]
+            )
+            VALUES
+            (
+                @NOME,
+                @EMAIL,
+                @TELEFONE,
+                @ENDERECO,
+                @CPF,
+                @CNH,
+                @VALIDADECNH
+            ); SELECT SCOPE_IDENTITY();";
+
+        protected override string sqlEditar =>
+            @"UPDATE [TBCONDUTOR] 
+            SET
+                [NOME] = @NOME,
+                [EMAIL] = @EMAIL,
+                [TELEFONE] = @TELEFONE,
+                [ENDERECO] = @ENDERECO,
+                [CPF] = @CPF,
+                [CNH] = @CNH,
+                [VALIDADECNH] = @VALIDADECNH
+            WHERE
+                [ID] = @ID";
+
+        protected override string sqlExcluir =>
+            @"DELETE FROM [TBCONDUTOR]                    
+            WHERE
+                [ID] = @ID";
+
+        protected override string sqlSelecionarTodos =>
+            @"SELECT 
+                [ID] CONDUTOR_ID, 
+                [NOME] CONDUTOR_NOME, 
+                [EMAIL] CONDUTOR_EMAIL,
+                [TELEFONE] CONDUTOR_TELEFONE,
+                [ENDERECO] CONDUTOR_ENDERECO,
+                [CPF] CONDUTOR_CPF,
+                [CNH] CONDUTOR_CNH,
+                [VALIDADECNH] CONDUTOR_VALIDADECNH
+            FROM 
+                [TBCONDUTOR]";
+
+        protected override string sqlSelecionarPorId =>
+            @"SELECT 
+                [ID] CONDUTOR_ID, 
+                [NOME] CONDUTOR_NOME, 
+                [EMAIL] CONDUTOR_EMAIL,
+                [TELEFONE] CONDUTOR_TELEFONE,
+                [ENDERECO] CONDUTOR_ENDERECO,
+                [CPF] CONDUTOR_CPF,
+                [CNH] CONDUTOR_CNH,
+                [VALIDADECNH] CONDUTOR_VALIDADECNH
+            FROM 
+                [TBCONDUTOR]
+            WHERE
+                [ID] = @ID";
+
+        private string sqlSelecionarPorCPF =>
+            @"SELECT 
+                [ID] CONDUTOR_ID, 
+                [NOME] CONDUTOR_NOME, 
+                [EMAIL] CONDUTOR_EMAIL,
+                [TELEFONE] CONDUTOR_TELEFONE,
+                [ENDERECO] CONDUTOR_ENDERECO,
+                [CPF] CONDUTOR_CPF,
+                [CNH] CONDUTOR_CNH,
+                [VALIDADECNH] CONDUTOR_VALIDADECNH
+            FROM 
+                [TBCONDUTOR]
+            WHERE
+                [CPF] = @CPF";
+
+        private string sqlSelecionarPorNome =>
+            @"SELECT 
+                [ID] CONDUTOR_ID, 
+                [NOME] CONDUTOR_NOME, 
+                [EMAIL] CONDUTOR_EMAIL,
+                [TELEFONE] CONDUTOR_TELEFONE,
+                [ENDERECO] CONDUTOR_ENDERECO,
+                [CPF] CONDUTOR_CPF,
+                [CNH] CONDUTOR_CNH,
+                [VALIDADECNH] CONDUTOR_VALIDADECNH
+            FROM 
+                [TBCONDUTOR]
+            WHERE
+                [NOME] = @NOME";
+
+        public Condutor SelecionarCondutorPorCPF(string cpf)
+        {
+            return SelecionarPorParametro(sqlSelecionarPorCPF, new SqlParameter("CPF", cpf));
+        }
+
+        public Condutor SelecionarCondutorPorNome(string nome)
+        {
+            return SelecionarPorParametro(sqlSelecionarPorNome, new SqlParameter("NOME", nome));
+        }
+    }
+}
