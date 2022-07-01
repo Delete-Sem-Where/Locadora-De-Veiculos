58693ec72ed758832d8bdbc6cccd4007b3ced6f4
Luciano Dalmollin <xlucianox811@gmail.com>
2022-07-01 18:52:42 -0300

    feat: Importei a TBTaxa com Enum TipoCalculo

--- LocadoraDeVeiculosDb/dbo/Tables/TBTaxa.sql
+++ LocadoraDeVeiculosDb/dbo/Tables/TBTaxa.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[TBTaxa] (
     [Id]        INT           IDENTITY (1, 1) NOT NULL,
-    [Descricao] VARCHAR (150) NULL,
-    [Valor]     FLOAT (53)    NULL,
+    [Descricao] VARCHAR (150) NOT NULL,
+    [Valor]     FLOAT (53)    NOT NULL,
+    [TipoCalculo]		  INT NOT NULL, 
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
