228b687499cc3c520cbd854f411444602b91201a
slackbot115 <viniccius.carvalho22@gmail.com>
2022-07-02 16:35:01 -0300

    feat: Adicionado Repositório e Mapeador de Cliente

---
+++ LocadoraDeVeiculos.Infra.BancoDados/ModuloCliente/MapeadorCliente.cs
@@ -0,0 +1,68 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloCliente;
+using LocadoraDeVeiculos.Infra.BancoDados.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.BancoDados.ModuloCliente
+{
+    public class MapeadorCliente : MapeadorBase<Cliente>
+    {
+        public override void ConfigurarParametros(Cliente registro, SqlCommand comando)
+        {
+            comando.Parameters.AddWithValue("ID", registro.Id);
+            comando.Parameters.AddWithValue("NOME", registro.Nome);
+            comando.Parameters.AddWithValue("EMAIL", registro.Email);
+            comando.Parameters.AddWithValue("TELEFONE", registro.Telefone);
+            comando.Parameters.AddWithValue("ENDERECO", registro.Endereco);
+            comando.Parameters.AddWithValue("TIPO_CLIENTE", registro.TipoCliente);
+
+            if (registro.TipoCliente == TipoCliente.PessoaJuridica)
+                comando.Parameters.AddWithValue("DOCUMENTO", registro.CNPJ);
+            else
+                comando.Parameters.AddWithValue("DOCUMENTO", registro.CPF);
+        }
+
+        public override Cliente ConverterRegistro(SqlDataReader leitorRegistro)
+        {
+            var id = Convert.ToInt32(leitorRegistro["CLIENTE_ID"]);
+            var nome = Convert.ToString(leitorRegistro["CLIENTE_NOME"]);
+            var email = Convert.ToString(leitorRegistro["CLIENTE_EMAIL"]);
+            var telefone = Convert.ToString(leitorRegistro["CLIENTE_TELEFONE"]);
+            var endereco = Convert.ToString(leitorRegistro["CLIENTE_ENDERECO"]);
+            var tipo = Convert.ToInt32(leitorRegistro["CLIENTE_TIPO_CLIENTE"]);
+            var documento = Convert.ToString(leitorRegistro["CLIENTE_DOCUMENTO"]);
+
+            Cliente cliente = new Cliente()
+            {
+                Id = id,
+                Nome = nome,
+                Email = email,
+                Telefone = telefone,
+                Endereco = endereco
+            };
+
+            ConfigurarTipoCliente(tipo, documento, cliente);
+
+            return cliente;
+        }
+
+        private void ConfigurarTipoCliente(int tipo, string documento, Cliente cliente)
+        {
+            if (tipo == 0)
+            {
+                cliente.TipoCliente = TipoCliente.PessoaFisica;
+                cliente.CPF = documento;
+            }
+            else if (tipo == 1)
+            {
+                cliente.TipoCliente = TipoCliente.PessoaJuridica;
+                cliente.CNPJ = documento;
+            }
+        }
+
+    }
+}
---
+++ LocadoraDeVeiculos.Infra.BancoDados/ModuloCliente/RepositorioClienteEmBancoDados.cs
@@ -0,0 +1,77 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloCliente;
+using LocadoraDeVeiculos.Infra.BancoDados.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.BancoDados.ModuloCliente
+{
+    public class RepositorioClienteEmBancoDados :
+        RepositorioBase<Cliente, MapeadorCliente>,
+        IRepositorioCliente
+    {
+        protected override string sqlInserir =>
+            @"INSERT INTO [TBCLIENTE]
+            (
+                [NOME],
+                [DOCUMENTO],
+                [EMAIL],
+                [TELEFONE],
+                [ENDERECO],
+                [TIPO_CLIENTE]
+            )
+            VALUES
+            (
+                @NOME, 
+                @DOCUMENTO, 
+                @EMAIL,
+                @TELEFONE,
+                @ENDERECO,
+                @TIPO_CLIENTE
+            );SELECT SCOPE_IDENTITY();";
+
+        protected override string sqlEditar =>
+            @"UPDATE [TBCLIENTE]
+            SET
+                [NOME] = @NOME,
+                [DOCUMENTO] = @DOCUMENTO,
+                [EMAIL] = @EMAIL,
+                [TELEFONE] = @TELEFONE,
+                [ENDERECO] = @ENDERECO,
+                [TIPO_CLIENTE] = @TIPO_CLIENTE
+            WHERE
+                [ID] = @ID";
+
+        protected override string sqlExcluir =>
+            @"DELETE FROM [TBCLIENTE]
+                WHERE [ID] = @ID";
+
+        protected override string sqlSelecionarTodos =>
+            @"SELECT 
+                [ID] CLIENTE_ID,
+                [NOME] CLIENTE_NOME,
+                [DOCUMENTO] CLIENTE_DOCUMENTO,
+                [EMAIL] CLIENTE_EMAIL,
+                [TELEFONE] CLIENTE_TELEFONE,
+                [ENDERECO] CLIENTE_ENDERECO,
+                [TIPO_CLIENTE] CLIENTE_TIPO_CLIENTE
+            FROM
+                [TBCLIENTE]";
+
+        protected override string sqlSelecionarPorId =>
+            @"SELECT 
+                [ID] CLIENTE_ID,
+                [NOME] CLIENTE_NOME,
+                [DOCUMENTO] CLIENTE_DOCUMENTO,
+                [EMAIL] CLIENTE_EMAIL,
+                [TELEFONE] CLIENTE_TELEFONE,
+                [ENDERECO] CLIENTE_ENDERECO,
+                [TIPO_CLIENTE] CLIENTE_TIPO_CLIENTE
+            FROM
+                [TBCLIENTE]
+            WHERE
+                [ID] = @ID";
+    }
+}
