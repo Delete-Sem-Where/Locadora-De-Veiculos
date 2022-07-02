cfcd8e75cf7651fb3b8e59db913e80c5c87c0684
slackbot115 <viniccius.carvalho22@gmail.com>
2022-07-02 16:36:34 -0300

    feat: Adicionado Tabela Cliente no Projeto de banco de dados

---
+++ LocadoraDeVeiculosDb/dbo/Tables/TBCliente.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TBCliente] (
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
+    [Nome]         VARCHAR (70)  NOT NULL,
+    [Documento]    VARCHAR (30)  NOT NULL,
+    [Email]        VARCHAR (70)  NOT NULL,
+    [Telefone]     VARCHAR (30)  NOT NULL,
+    [Endereco]     VARCHAR (100) NOT NULL,
+    [Tipo_Cliente] INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
