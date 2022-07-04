3adc978e62fc35810b07182b03e08fa2e55fceba
Luciano Dalmollin <xlucianox811@gmail.com>
2022-07-04 13:36:37 -0300

    feat: Dominio do PlanoCobranca

---
+++ LocadoraDeVeiculos.Dominio/ModuloPlanoCobranca/ValidadorPlanoCobranca.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloPlanoCobranca
+{
+    public class ValidadorPlanoCobranca : AbstractValidator<PlanoCobranca>
+    {
+        public ValidadorPlanoCobranca()
+        {
+            //RuleFor(x => x.Nome)
+            //    .NotNull().NotEmpty()
+            //    .Nomes();
+
+            //RuleFor(x => x.Email)
+            //    .NotNull().NotEmpty()
+            //    .EmailAddress();
+
+            //RuleFor(x => x.Telefone)
+            //    .NotNull().NotEmpty()
+            //    .Telefone();
+
+            //RuleFor(x => x.Endereco)
+            //    .NotNull().NotEmpty()
+            //    .Endereco();
+
+            //When(x => x.TipoPlanoCobranca == TipoPlanoCobranca.PessoaJuridica, () =>
+            //{
+            //    RuleFor(x => x.CNPJ)
+            //    .NotNull().NotEmpty()
+            //    .CNPJ();
+
+            //});
+
+            //When(x => x.TipoPlanoCobranca == TipoPlanoCobranca.PessoaFisica, () =>
+            //{
+            //    RuleFor(x => x.CPF)
+            //    .NotNull().NotEmpty()
+            //    .CPF();
+            //});
+        }
+    }
+}
