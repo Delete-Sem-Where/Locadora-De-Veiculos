d040cb0ac9ac6372ff6a9c63ac3eefed8f52c33d
slackbot115 <viniccius.carvalho22@gmail.com>
2022-07-12 16:45:14 -0300

    refact: Alterado INT para Guid nas tabelas Cliente, Condutor e Funcionário

--- LocadoraDeVeiculosDb/dbo/Tables/TBFuncionario.sql
+++ LocadoraDeVeiculosDb/dbo/Tables/TBFuncionario.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE [dbo].[TBFuncionario] (
-    [Id]             INT            IDENTITY (1, 1) NOT NULL,
-    [Nome]           VARCHAR (70)   NOT NULL,
-    [Email]          VARCHAR (70)   NOT NULL,
-    [Telefone]       VARCHAR (20)   NOT NULL,
-    [Endereco]       VARCHAR (70)   NOT NULL,
-    [Login]          VARCHAR (50)   NOT NULL,
-    [Senha]          VARCHAR (50)   NOT NULL,
-    [Salario]        DECIMAL (8, 2) NOT NULL,
-    [DataDeAdmissao] DATE           NOT NULL,
-    CONSTRAINT [PK_TBFuncionario] PRIMARY KEY CLUSTERED ([Id] ASC)
+    [Id]             UNIQUEIDENTIFIER NOT NULL,
+    [Nome]           VARCHAR (70)     NOT NULL,
+    [Email]          VARCHAR (70)     NOT NULL,
+    [Telefone]       VARCHAR (20)     NOT NULL,
+    [Endereco]       VARCHAR (70)     NOT NULL,
+    [Login]          VARCHAR (50)     NOT NULL,
+    [Senha]          VARCHAR (50)     NOT NULL,
+    [Salario]        DECIMAL (8, 2)   NOT NULL,
+    [DataDeAdmissao] DATE             NOT NULL,
+    CONSTRAINT [PK_TBFuncionario_1] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
