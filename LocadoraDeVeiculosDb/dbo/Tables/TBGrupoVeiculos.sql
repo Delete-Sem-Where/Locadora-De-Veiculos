4ee0efd67c440af9cf1e82b575c6c5a9d4f4456b
luciano811 <xlucianox811@gmail.com>
2022-07-13 15:53:12 -0300

    refact: Implementado Guid nas tabelas Plano de Cobrança e Taxas

--- LocadoraDeVeiculosDb/dbo/Tables/TBGrupoVeiculos.sql
+++ LocadoraDeVeiculosDb/dbo/Tables/TBGrupoVeiculos.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[TBGrupoVeiculos] (
-    [Id]   INT          IDENTITY (1, 1) NOT NULL,
-    [Nome] VARCHAR (50) NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
+    [Id]   UNIQUEIDENTIFIER NOT NULL,
+    [Nome] VARCHAR (50)     NOT NULL,
+    CONSTRAINT [PK_TBGrupoVeiculos] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
