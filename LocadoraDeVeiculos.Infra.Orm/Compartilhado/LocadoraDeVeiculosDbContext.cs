c9a42d74a6ae39d1c29c710d139ff1d0d1073b14
slackbot115 <viniccius.carvalho22@gmail.com>
2022-07-26 09:21:50 -0300

    feat: Iniciado implementação de ORM

---
+++ LocadoraDeVeiculos.Infra.Orm/Compartilhado/LocadoraDeVeiculosDbContext.cs
@@ -0,0 +1,49 @@
+﻿using LocadoraDeVeiculos.Dominio.Compartilhado;
+using LocadoraDeVeiculos.Dominio.ModuloCliente;
+using LocadoraDeVeiculos.Dominio.ModuloCondutor;
+using LocadoraDeVeiculos.Dominio.ModuloFuncionario;
+using LocadoraDeVeiculos.Dominio.ModuloGruposVeiculos;
+using LocadoraDeVeiculos.Dominio.ModuloPlanoCobranca;
+using LocadoraDeVeiculos.Dominio.ModuloTaxas;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace LocadoraDeVeiculos.Infra.Orm.Compartilhado
+{
+    public class LocadoraDeVeiculosDbContext : DbContext, IContextoPersistencia
+    {
+        private string connectionString;
+
+        public LocadoraDeVeiculosDbContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void GravarDados()
+        {
+            SaveChanges();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+
+            ILoggerFactory loggerFactory = LoggerFactory.Create((x) =>
+            {
+                x.AddSerilog(Log.Logger);
+            });
+
+            optionsBuilder.UseLoggerFactory(loggerFactory);
+
+            optionsBuilder.EnableSensitiveDataLogging();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var dllComConfiguracoesOrm = typeof(LocadoraDeVeiculosDbContext).Assembly;
+
+            modelBuilder.ApplyConfigurationsFromAssembly(dllComConfiguracoesOrm);
+        }
+    }
+}
---
+++ LocadoraDeVeiculos.Infra.Orm/Compartilhado/LocadoraDeVeiculosDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace LocadoraDeVeiculos.Infra.Orm.Compartilhado
+{
+    public class LocadoraDeVeiculosDbContextFactory : IDesignTimeDbContextFactory<LocadoraDeVeiculosDbContext>
+    {
+        public LocadoraDeVeiculosDbContext CreateDbContext(string[] args)
+        {
+            var configuracao = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("ConfiguracaoAplicacao.json")
+             .Build();
+
+            var connectionString = configuracao.GetConnectionString("SqlServer");
+
+            return new LocadoraDeVeiculosDbContext(connectionString);
+        }
+    }
+}
