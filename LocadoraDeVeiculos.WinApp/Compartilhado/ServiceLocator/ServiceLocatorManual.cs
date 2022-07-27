05fd4117a9abc4ec81856d0fdc961b6c5e2b2f72
slackbot115 <viniccius.carvalho22@gmail.com>
2022-07-26 23:47:56 -0300

    refact: Alterado ServiceLocator para Manual

---
+++ LocadoraDeVeiculos.WinApp/Compartilhado/ServiceLocator/ServiceLocatorManual.cs
@@ -0,0 +1,64 @@
+﻿using LocadoraDeVeiculos.Aplicacao.ModuloCliente;
+using LocadoraDeVeiculos.Aplicacao.ModuloCondutor;
+using LocadoraDeVeiculos.Aplicacao.ModuloFuncionario;
+using LocadoraDeVeiculos.Infra.Orm.Compartilhado;
+using LocadoraDeVeiculos.Infra.Orm.ModuloCliente;
+using LocadoraDeVeiculos.Infra.Orm.ModuloCondutor;
+using LocadoraDeVeiculos.Infra.Orm.ModuloFuncionario;
+using LocadoraDeVeiculos.WinApp.ModuloCliente;
+using LocadoraDeVeiculos.WinApp.ModuloCondutor;
+using LocadoraDeVeiculos.WinApp.ModuloFuncionario;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.WinApp.Compartilhado.ServiceLocator
+{
+    public class ServiceLocatorManual : IServiceLocator
+    {
+        private Dictionary<string, ControladorBase> controladores;
+
+        public ServiceLocatorManual()
+        {
+            controladores = new Dictionary<string, ControladorBase>();
+
+            ConfigurarControladores();
+        }
+
+        public T Get<T>() where T : ControladorBase
+        {
+            var tipo = typeof(T);
+
+            var nomeControlador = tipo.Name;
+
+            return (T)controladores[nomeControlador];
+        }
+
+        private void ConfigurarControladores()
+        {
+            var configuracao = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("ConfiguracaoAplicacao.json")
+                 .Build();
+
+            var connectionString = configuracao.GetConnectionString("SqlServer");
+
+            var contextoDadosOrm = new LocadoraDeVeiculosDbContext(connectionString);
+
+            var repositorioFuncionario = new RepositorioFuncionarioOrm(contextoDadosOrm);
+            var servicoFuncionario = new ServicoFuncionario(repositorioFuncionario, contextoDadosOrm);
+            controladores.Add("ControladorFuncionario", new ControladorFuncionario(servicoFuncionario));
+
+            var repositorioCliente = new RepositorioClienteOrm(contextoDadosOrm);
+            var servicoCliente = new ServicoCliente(repositorioCliente, contextoDadosOrm);
+            controladores.Add("ControladorCliente", new ControladorCliente(servicoCliente));
+
+            var repositorioCondutor = new RepositorioCondutorOrm(contextoDadosOrm);
+            var servicoCondutor = new ServicoCondutor(repositorioCondutor, contextoDadosOrm);
+            controladores.Add("ControladorCondutor", new ControladorCondutor(servicoCondutor, servicoCliente));
+        }
+    }
+}
